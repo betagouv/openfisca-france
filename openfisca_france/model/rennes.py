7ada071d74595eab25e86c8a3c5ede73e109a372
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-08-05 14:47:03 +0200

    Add mock aide for Rennes

---
+++ openfisca_france/model/rennes.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+from openfisca_france.model.base import *  # noqa analysis:ignore
+
+class rennes_metropole_transport(Variable):
+    column = FloatCol
+    entity_class = Familles
+    label = u"Rennes"
